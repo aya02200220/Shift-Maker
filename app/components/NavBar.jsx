c4dd5c3f146c9f2fe4dc4072a84ac1dd64000df6
Aya <pyon220@gmail.com>
2024-04-14 21:20:50 -0700

    added p7 logo

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -11,7 +11,7 @@ import Autocomplete from "@mui/material/Autocomplete";
 export default function Navbar() {
   const { status, data: session } = useSession();
   return (
-    <div className="p-4 flex justify-between items-center shadow-md">
+    <div className="px-4 py-2 flex justify-between items-center shadow-md">
       <Link className="font-bold text-lg text-blue-700" href={"/"}>
         <Image
           className="rounded-full mr-2"
@@ -22,18 +22,19 @@ export default function Navbar() {
       </Link>
       {status === "authenticated" ? (
         <div className="flex justify-center items-center gap-2">
-          <div>
+          <Image
+            className="rounded-full "
+            src={session?.user?.image}
+            width={30}
+            height={30}
+          />
+          <div className="mr-2">
             <div>
               Hello! <span className="font-bold">{session?.user?.name}</span>
             </div>
             <div className="text-[10px]">{session?.user?.email}</div>
           </div>
-          <Image
-            className="rounded-full mr-2"
-            src={session?.user?.image}
-            width={30}
-            height={30}
-          />
+
           <button
             onClick={() => signOut()}
             className="bg-slate-900 text-white px-3 py-1 rounded-md text-[12px]"
