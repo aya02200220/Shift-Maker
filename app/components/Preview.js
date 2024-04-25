7717f1ee2f41474850c46fd86daf7f3b8e7893ab
Aya <pyon220@gmail.com>
2024-04-24 22:08:19 -0700

    added screenshot function

--- app/components/Preview.js
+++ app/components/Preview.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef, createRef } from "react";
+import { useScreenshot } from "use-react-screenshot";
 
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
@@ -22,6 +23,12 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 
 export default function Preview({}) {
   const [open, setOpen] = useState(false);
+
+  const ref = createRef(null);
+  const [width, setWidth] = useState(300);
+  const [image, takeScreenShot] = useScreenshot();
+
+  const getImage = () => takeScreenShot(ref.current);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -68,25 +75,104 @@ export default function Preview({}) {
           <CloseIcon />
         </IconButton>
         <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
-          </Typography>
+          {/* <img width={width} src={image} alt={"Screenshot"} /> */}
+          <div ref={ref}></div>
+
+          <div>
+            <div>
+              {/* <Button
+                variant="contained"
+                style={{ marginBottom: "10px" }}
+                onClick={getImage}
+              >
+                Take screenshot
+              </Button> */}
+              <label style={{ display: "block", margin: "10px 0" }}>
+                Width:
+                <input
+                  value={width}
+                  onChange={(e) => setWidth(e.target.value)}
+                />
+              </label>
+            </div>
+            <img width={width} src={image} alt={"ScreenShot"} />
+            <div
+              ref={ref}
+              style={{
+                border: "1px solid #ccc",
+                padding: "10px",
+                marginTop: "20px",
+              }}
+            >
+              <div>
+                <p className="text-[10px]">
+                  What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+                  printing and typesetting industry. Lorem Ipsum has been the
+                  industry's standard dummy text ever since the 1500s, when an
+                  unknown printer took a galley of type and scrambled it to make
+                  a type specimen book. It has survived not only five centuries,
+                  but also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum. Why do
+                  we use it? It is a long established fact that a reader will be
+                  distracted by the readable content of a page when looking at
+                  its layout. The point of using Lorem Ipsum is that it has a
+                  more-or-less normal distribution of letters, as opposed to
+                  using 'Content here, content here', making it look like
+                  readable English. Many desktop publishing packages and web
+                  page editors now use Lorem Ipsum as their default model text,
+                  and a search for 'lorem ipsum' will uncover many web sites
+                  still in their infancy. Various versions have evolved over the
+                  years, sometimes by accident, sometimes on purpose (injected
+                  humour and the like). Where does it come from? Contrary to
+                  popular belief, Lorem Ipsum is not simply random text. It has
+                  roots in a piece of classical Latin literature from 45 BC,
+                  making it over 2000 years old. Richard McClintock, a Latin
+                  professor at Hampden-Sydney College in Virginia, looked up one
+                  of the more obscure Latin words, consectetur, from a Lorem
+                  Ipsum passage, and going through the cites of the word in
+                  classical literature, discovered the undoubtable source. Lorem
+                  Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
+                  Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
+                  written in 45 BC. This book is a treatise on the theory of
+                  ethics, very popular during the Renaissance. The first line of
+                  Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
+                  in section 1.10.32. The standard chunk of Lorem Ipsum used
+                  since the 1500s is reproduced below for those interested.
+                  Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+                  Malorum" by Cicero are also reproduced in their exact original
+                  form, accompanied by English versions from the 1914
+                  translation by H. Rackham. Where can I get some? There are
+                  many variations of passages of Lorem Ipsum available, but the
+                  majority have suffered alteration in some form, by injected
+                  humour, or randomised words which don't look even slightly
+                  believable. If you are going to use a passage of Lorem Ipsum,
+                  you need to be sure there isn't anything embarrassing hidden
+                  in the middle of text. All the Lorem Ipsum generators on the
+                  Internet tend to repeat predefined chunks as necessary, making
+                  this the first true generator on the Internet. It uses a
+                  dictionary of over 200 Latin words, combined with a handful of
+                  model sentence structures, to generate Lorem Ipsum which looks
+                  reasonable. The generated Lorem Ipsum is therefore always free
+                  from repetition, injected humour, or non-characteristic words
+                  etc.
+                </p>
+              </div>
+            </div>
+          </div>
         </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={handleClose}>
+          {/* <Button autoFocus onClick={handleClose}>
             Save changes
+          </Button> */}
+          <Button
+            variant="contained"
+            style={{ marginBottom: "10px" }}
+            onClick={getImage}
+          >
+            Take screenshot
           </Button>
         </DialogActions>
       </BootstrapDialog>
