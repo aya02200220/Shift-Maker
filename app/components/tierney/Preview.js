bbc3abbbd6c5d651d98d0df324900b1e41464cb9
Aya <pyon220@gmail.com>
2024-04-30 11:47:58 -0700

    added preview.js on tiernys

--- app/components/tierney/Preview.js
+++ app/components/tierney/Preview.js
@@ -1,44 +1,111 @@
 //tierney/Preview.js
 import React, { useState, useRef, createRef, useEffect } from "react";
-import { useScreenshot, createFileName } from "use-react-screenshot";
+import dayjs from "dayjs";
+// import CreateEmail from "./mails/CreateEmail";
+import SendEmail from "../mails/SendEmail";
+
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import Divider from "@mui/material/Divider";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import DatePickers from "./DatePickers";
 
 import { format } from "date-fns";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
-import { Divider } from "@mui/material";
+import { useScreenshot, createFileName } from "use-react-screenshot";
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1),
-  },
-}));
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function Preview({ previousOrder, previousOrderDate }) {
   const [open, setOpen] = useState(false);
-  const [orderDetail, setOrderDetail] = useState(previousOrder);
-  const [orderDate, setOrderDate] = useState(previousOrderDate);
 
-  const formattedDate = orderDate ? format(orderDate, "MMMM d, yyyy") : "";
-  const formattedDateForDl = orderDate ? format(orderDate, "MMdd") : "";
+  const [orderDetail, setOrderDetail] = useState(previousOrder);
+  const [orderDate, setOrderDate] = useState(null);
+
+  const formattedDate = orderDate
+    ? format(orderDate.toDate(), "MMMM d, yyyy")
+    : "";
+  const formattedDateForDl = orderDate
+    ? format(orderDate.toDate(), "MMdd")
+    : "";
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     setOrderDetail(previousOrder);
   }, [previousOrder]);
 
   useEffect(() => {
-    setOrderDate(previousOrderDate);
+    if (previousOrderDate) {
+      const parsedDate = dayjs(previousOrderDate);
+      if (parsedDate.isValid()) {
+        setOrderDate(parsedDate);
+      } else {
+        console.error("Invalid date format:", previousOrderDate);
+      }
+    } else {
+      console.error("Empty date:", previousOrderDate);
+    }
   }, [previousOrderDate]);
+
+  const handleDateChange = (date) => {
+    console.log("Selected date:", date);
+    setOrderDate(date);
+  };
+
+  useEffect(() => {
+    console.log("再選択されたDateでフェッチ", orderDate);
+    if (orderDate) {
+      const fetchDetails = async () => {
+        // 内部関数の名前を変更
+        // const isoDate = orderDate.toISOString();
+        const isoDate = orderDate.toDate().toISOString();
+        console.log("isoDate", isoDate);
+        const details = await fetchOrderDetails(orderDate); // 外部の fetchOrderDetails を呼び出し
+        console.log("details", details);
+        setOrderDetail(details);
+      };
+
+      fetchDetails().catch(console.error);
+    }
+  }, [orderDate]);
+
+  const fetchOrderDetails = async (date) => {
+    console.log("fetchOrderDetails", date);
+    try {
+      const response = await fetch(
+        `/api/order-find-by-date?date=${date.toISOString().slice(0, 10)}`
+      );
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(
+          errorData.message || "An error occurred while fetching order details"
+        );
+      }
+      const data = await response.json();
+      if (data.orders.length > 0) {
+        return data.orders[data.orders.length - 1].orderDetails; // 最後の注文のorderDetailsを取得
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error("Failed to fetch order details:", error);
+      return [];
+    }
+  };
 
   const ref = createRef(null);
   const [image, takeScreenShot] = useScreenshot({
@@ -56,12 +123,12 @@ export default function Preview({ previousOrder, previousOrderDate }) {
     a.click();
   };
   const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+  console.log("orderDetail", orderDetail);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
+  // オーダーがあるかどうかをチェックする
+  const getOrderStyle = (detail) => {
+    const isOrder = detail.order > 0;
+    return isOrder ? "bg-green-100" : "";
   };
 
   return (
@@ -69,7 +136,7 @@ export default function Preview({ previousOrder, previousOrderDate }) {
       <Button
         endIcon={<FileDownloadIcon />}
         variant="contained"
-        color="success"
+        color="secondary"
         onClick={handleClickOpen}
       >
         <Typography className="leading-3 text-[12px] font-medium w-[80px] h-[30px] flex justify-center items-center">
@@ -79,33 +146,53 @@ export default function Preview({ previousOrder, previousOrderDate }) {
         </Typography>
       </Button>
 
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
+      <Dialog
+        fullScreen
         open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
       >
-        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Modal title
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent dividers>
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar className=" bg-[#d1a0b7]">
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Preview
+            </Typography>
+
+            <div className="flex gap-2">
+              {/* <CreateEmail orderDate={orderDate} /> */}
+              <SendEmail orderDate={orderDate} />
+
+              <Button
+                endIcon={<FileDownloadIcon />}
+                variant="contained"
+                className="text-[12px] bg-[#b2698b] hover:bg-[#b64f7f] "
+                onClick={downloadScreenshot}
+              >
+                Download
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div>
+          <DatePickers orderDate={orderDate} onDateChange={handleDateChange} />
+          <Divider />
           <div ref={ref}></div>
 
-          <div>
+          <div
+            className="flex justify-center
+          "
+          >
             <div
               ref={ref}
-              className="p-2 mt-2 border rounded-md shadow-md max-w-[450px]"
+              className="p-2 mt-2 border rounded-md shadow-md max-w-[470px] bg-[#fff]"
             >
               <div>
                 <h3 className="text-[16px] md:text-md font-semibold text-center md:text-left mb-2">
@@ -114,121 +201,59 @@ export default function Preview({ previousOrder, previousOrderDate }) {
                 <Divider />
                 <div>
                   {orderDetail && (
-                    <div>
-                      <ul className="flex text-[14px] text-[#333] font-medium items-center justify-center gap-1 md:gap-2 mb-0 md:mb-2 leading-3 mt-1 md:mt-2">
-                        <li className="w-[90px] md:w-[210px]"></li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
+                    <div className="flex flex-col justify-center">
+                      <ul className="flex text-[14px] text-[#333] font-medium items-center justify-between mr-2 md:nr-0 md:justify-center gap-0 md:gap-2 mb-0 md:mb-2 leading-3 mt-1 md:mt-2">
+                        <li className="w-[117px] md:w-[210px]"></li>
+                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
                           Unopened
                         </li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
+                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
                           Opened&nbsp;(%)
                         </li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
+                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
                           Tin&nbsp;(%)
                         </li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
+                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
                           Order
                         </li>
                       </ul>
                       <ul>
                         {orderDetail.map((detail, index) => (
                           <li
-                            className="flex justify-between border-b-2 h-[30px] text-[14.5px] items-center px-2  pb-1"
+                            className={`flex justify-between border-b-2 h-[18px] md:h-[30px] text-[14.5px] items-center px-2  pb-1
+                            ${getOrderStyle(orderDetail[index])}`}
                             key={index}
                           >
-                            <p className="w-[98px] md:w-[165px] mr-2 leading-[9px] text-[#333] text-[11px] md:text-[13px]">
+                            <p className="w-[98px] md:w-[165px] mr-2 leading-[9px] text-[#333] text-[8px] md:text-[13px]">
                               {detail.teaName}
                             </p>
 
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
+                            <p className="w-[43px] text-center  text-[#999] text-[8px] md:text-[13px] md:ml-3">
                               {detail.unopened}
                             </p>
 
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
+                            <p className="w-[43px] text-center  text-[#999] text-[8px] md:text-[13px] md:ml-3">
                               {detail.opened}
                             </p>
 
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
+                            <p className="w-[43px] text-center  text-[#999] text-[8px] md:text-[13px] md:ml-3">
                               {detail.tin}
                             </p>
 
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
+                            <p className="w-[43px] text-center  text-[#999] text-[8px] md:text-[13px] md:ml-3">
                               {detail.order}
                             </p>
                           </li>
                         ))}
                       </ul>
-                      {/* <ul className="flex text-[14px] text-[#333] font-medium items-center justify-center gap-1 md:gap-2 mb-0 md:mb-2 leading-3 mt-1 md:mt-2">
-                        <li className="w-[90px] md:w-[210px]"></li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
-                          Unopened
-                        </li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
-                          Opened&nbsp;(%)
-                        </li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
-                          Tin&nbsp;(%)
-                        </li>
-                        <li className="text-[10px] md:text-[11px] w-[42px] md:w-[68px] text-center break-words">
-                          Order
-                        </li>
-                      </ul>
-                      <ul>
-                        {orderDetail.map((detail, index) => (
-                          <li
-                            className="flex justify-between border-b-2 h-[30px] text-[14.5px] items-center px-2  pb-1"
-                            key={index}
-                          >
-                            <p className="w-[98px] md:w-[165px] mr-2 leading-[9px] text-[#333] text-[11px] md:text-[13px]">
-                              {detail.teaName}
-                            </p>
-
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
-                              {detail.unopened}
-                            </p>
-
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
-                              {detail.opened}
-                            </p>
-
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
-                              {detail.tin}
-                            </p>
-
-                            <p className="w-[43px] text-center  text-[#999] text-[11px] md:ml-3">
-                              {detail.order}
-                            </p>
-                          </li>
-                        ))}
-                      </ul> */}
                     </div>
                   )}
                 </div>
               </div>
             </div>
           </div>
-        </DialogContent>
-        <DialogActions>
-          {/* <Button autoFocus onClick={handleClose}>
-            Save changes
-          </Button> */}
-          <Button
-            endIcon={<FileDownloadIcon />}
-            variant="contained"
-            style={{ marginBottom: "10px", fontSize: "12px" }}
-            onClick={downloadScreenshot}
-          >
-            Download
-          </Button>
-          {/* <Button
-            variant="contained"
-            style={{ marginBottom: "10px" }}
-            onClick={getImage}
-          >
-            Take screenshot
-          </Button> */}
-        </DialogActions>
-      </BootstrapDialog>
+        </div>
+      </Dialog>
     </>
   );
 }
