76aec8eb8f5a5da0694ec7168c7c88e2359b0a8e
Aya <pyon220@gmail.com>
2024-05-20 23:00:00 -0700

    added design on oreview page - tierneys

--- app/components/tierney/Preview.js
+++ app/components/tierney/Preview.js
@@ -188,53 +188,42 @@ export default function PreviewFullScreen({
               className="p-2 mt-2 border rounded-md shadow-md max-w-[470px] bg-[#fff]"
             >
               <div>
-                <h3 className="text-[16px] md:text-md font-semibold text-center md:text-left mb-2">
+                <h3 className="text-[16px] sm:text-md font-semibold text-center sm:text-left mb-2">
                   Order Date: {formattedDate}
                 </h3>
                 <Divider />
                 <div>
                   {orderDetail && (
                     <div className="flex flex-col justify-center">
-                      <ul className="flex text-[14px] text-[#333] font-medium items-center justify-between mr-2 md:nr-0 md:justify-center gap-0 md:gap-2 mb-0 md:mb-2 leading-3 mt-1 md:mt-2">
-                        <li className="w-[117px] md:w-[210px]"></li>
-                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
-                          Unopened
+                      <ul className="flex text-[14px] text-[#333] font-medium items-center justify-between mr-2 sm:nr-0 sm:justify-center gap-0 sm:gap-2 mb-0 sm:mb-2 leading-3 mt-1 sm:mt-2">
+                        <li className="text-[8px] sm:text-[11px] w-[140px] sm:w-[165px] text-center break-words">
+                          Item
                         </li>
-                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
-                          Opened&nbsp;(%)
+                        <li className="text-[8px] sm:text-[11px] w-[50px] break-words ml-3 text-right">
+                          Price
                         </li>
-                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
-                          Tin&nbsp;(%)
-                        </li>
-                        <li className="text-[8px] md:text-[11px] w-[46px] md:w-[68px] text-center break-words">
+
+                        <li className="text-[8px] sm:text-[11px] w-[40px] text-center break-words ml-3">
                           Order
                         </li>
                       </ul>
                       <ul>
                         {orderDetail.map((detail, index) => (
                           <li
-                            className={`flex justify-between border-b-2 h-[18px] md:h-[30px] text-[14.5px] items-center px-2 pb-1 ${getOrderStyle(
+                            className={`flex justify-between border-b-2 h-[25px] sm:h-[30px] text-[14.5px] items-center px-2 pb-1 ${getOrderStyle(
                               detail
                             )}`}
                             key={index}
                           >
-                            <p className="w-[98px] md:w-[165px] mr-2 leading-[9px] text-[#333] text-[8px] md:text-[13px]">
-                              {detail.teaName}
+                            <p className="w-[140px] sm:w-[165px] mr-2 leading-[9px] text-[#333] text-[11px] sm:text-[13px]">
+                              {detail.displayName}
                             </p>
 
-                            <p className="w-[43px] text-center text-[#999] text-[8px] md:text-[13px] md:ml-3">
-                              {detail.unopened}
+                            <p className="w-[50px] text-right text-[#999] text-[11px] sm:text-[13px] ml-3">
+                              ${detail.price.toFixed(2)}
                             </p>
 
-                            <p className="w-[43px] text-center text-[#999] text-[8px] md:text-[13px] md:ml-3">
-                              {detail.opened}
-                            </p>
-
-                            <p className="w-[43px] text-center text-[#999] text-[8px] md:text-[13px] md:ml-3">
-                              {detail.tin}
-                            </p>
-
-                            <p className="w-[43px] text-center text-[#999] text-[8px] md:text-[13px] md:ml-3">
+                            <p className="w-[40px] text-center text-[#999] text-[11px] sm:text-[13px] ml-3">
                               {detail.order}
                             </p>
                           </li>
